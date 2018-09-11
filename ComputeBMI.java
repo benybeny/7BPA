4a82a48204d04c18e7859997b0f185a4bfc08b28
benybeny <benyoohhh@gmail.com>
2018-09-10 22:45:25 -0500

    actividad en clase

---
+++ ComputeBMI.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class ComputeBMI{
+  public static void main(String[] args) {
+    Scanner input=new Scanner(System.in);
+    //palabra para las constantes  con un palabra reservada
+        final double KILOGRAMS_PER_POUND = 0.4535923;
+        final double  METERS_PER_INCH=0.254;
+
+    System.out.println("Enter weight in pounds:");
+    double weight=input.nextDouble();
+
+    System.out.println("Enter weight in inches:");
+    double height=input.nextDouble();
+
+double weightInKilogams= weight*  KILOGRAMS_PER_POUND;
+double heightInMeters=height * METERS_PER_INCH;
+
+
+
+
+
+
+  }
+}
