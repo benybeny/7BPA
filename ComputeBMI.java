05586c6add67a9497299162956f9f10292d7b730
benybeny <benyoohhh@gmail.com>
2018-09-17 17:41:54 -0500

    ejercicio

--- ComputeBMI.java
+++ ComputeBMI.java
@@ -15,19 +15,22 @@ public class ComputeBMI{
 double weightInKilogams= weight*  KILOGRAMS_PER_POUND;
 double heightInMeters=height * METERS_PER_INCH;
 
-double BMI=weightInKilogams/(heightInMeters*heightInMeters);
+double bmi=weightInKilogams/(heightInMeters*heightInMeters);
+//double bmi =weightInKilogams/Math.pow (heightInMeters,2 )
+System.out.println( "your BMI is " + bmi );
 
-if (BMI<18) {
-  System.out.println("Peso bajo.Necesario valorar signos de nutricion ");
-}else { if (BMI>=18 && BMI<=24.5) {
-  System.out.println( "holaalal");
-
-}
-
-}
-
-
-
+if (bmi < 16 )
+System.out.println("your are seiuosly underweight");
+else if (bmi < 18)
+System.out.println("your are underweight ");
+else if (bmi < 24 )
+System.out.println("your are normal weight");
+else if (bmi <29 )
+System.out.println("your are overweight ");
+else if (bmi <35 )
+System.out.println("your are seriously overweight");
+else
+System.out.println("your are gravely overweight");
 
   }
 }
